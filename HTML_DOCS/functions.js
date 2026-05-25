160b55ae53259333917574d492437ef2cd824e2a
12401453 <12401453@protonmail.com>
2026-05-25 06:01:51 +0100

    ..

--- HTML_DOCS/functions.js
+++ HTML_DOCS/functions.js
@@ -3084,11 +3084,11 @@ const displayDictBox = (error_msg, event_target=null) => {
 
       
       if(conj_type != undefined && conj_type != "non_infl") {
-        const dict_conj_table_btn_html_str = `<div id="dict_conj_table_btn" data-lemma_id="${lemma_id}" data-morph_tag="${morph_tag}" data-inflexion="${conj_type}" data-inflexion="${pv2_3_exists}" title="Show inflection table for this lemma"><svg id="grid_symbol" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        const dict_conj_table_btn_html_str = `<div id="dict_conj_table_btn" data-lemma_id="${lemma_id}" data-morph_tag="${morph_tag}" data-inflexion="${conj_type}" data-pv3="${pv2_3_exists}" title="Show inflection table for this lemma"><svg id="grid_symbol" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M9 4L9 20M15 4L15 20M3 9H21M3 15H21M6.2 20H17.8C18.9201 20 19.4802 20 19.908 19.782C20.2843 19.5903 20.5903 19.2843 20.782 18.908C21 18.4802 21 17.9201 21 16.8V7.2C21 6.0799 21 5.51984 20.782 5.09202C20.5903 4.71569 20.2843 4.40973 19.908 4.21799C19.4802 4 18.9201 4 17.8 4H6.2C5.07989 4 4.51984 4 4.09202 4.21799C3.71569 4.40973 3.40973 4.71569 3.21799 5.09202C3 5.51984 3 6.07989 3 7.2V16.8C3 17.9201 3 18.4802 3.21799 18.908C3.40973 19.2843 3.71569 19.5903 4.09202 19.782C4.51984 20 5.07989 20 6.2 20Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg></div>`;
         const conj_table_symbol = document.createRange().createContextualFragment(dict_conj_table_btn_html_str);
-        conj_table_symbol.getElementById("dict_conj_table_btn").addEventListener('click', showWordInfoBox, true);
+        conj_table_symbol.getElementById("dict_conj_table_btn").addEventListener('click', showWordInfoBox);
         snsp_button.after(conj_table_symbol);
       }
     }
